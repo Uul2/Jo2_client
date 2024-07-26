1361e8cdf2edea39628cfeb6b4d76fefcd5b50d0
박성재 <sem201@naver.com>
2024-07-26 11:18:06 +0900

    feat: header 작업

---
+++ src/components/common/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import * as S from "./styled"
+
+const Header=()=>{
+    return(
+        <S.HeadContainer>우울해하지마요 덮밥</S.HeadContainer>
+    )
+}
+
+
+export default Header;
---
+++ src/pages/Chatpage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Chatpage=()=>{
+    return(
+        <></>
+    )
+}
+
+export default Chatpage;
